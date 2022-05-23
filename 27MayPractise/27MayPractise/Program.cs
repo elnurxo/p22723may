6cd868e0044f0e5cf39dfb3744d75dcc1e3c51a0
elnurxo <elnurykh@code.edu.az>
2022-05-23 17:05:43 +0400

    my first commit

---
+++ 27MayPractise/27MayPractise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _27MayPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello P227!");
+        }
+    }
+}
